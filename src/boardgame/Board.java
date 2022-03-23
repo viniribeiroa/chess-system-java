f134558828fb6e8c0d3c643aa3ab7ddc37c191d6
Vinicius Alves Ribeiro <viniribeiroa@gmail.com>
2022-03-23 12:13:58 -0300

    criado as exceções

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -9,6 +9,9 @@ public class Board {
 	
 	public Board(int linhas, int colunas) {
 		
+		if(linhas < 1 || colunas < 1 ) {
+			throw new BoardException("ERRO ao criar tabuleiro é necessario que seja criado pelo menos uma linha e uma coluna");
+		}
 		this.linhas = linhas;
 		this.colunas = colunas;
 		pecas = new Peca[linhas][colunas];
@@ -19,31 +22,51 @@ public class Board {
 		return linhas;
 	}
 
-
-	public void setLinhas(int linhas) {
-		this.linhas = linhas;
-	}
-
-
+	
 	public int getColunas() {
 		return colunas;
 	}
 
-
-	public void setColunas(int colunas) {
-		this.colunas = colunas;
-	}
-	
+		
 	public Peca peca(int linha, int coluna) {
+		
+		if(!positionExists(linha, coluna)) {
+			throw new BoardException("Esta posição não existe no tabuleiro");
+		}
 		return pecas[linha][coluna];
 	}
 	public Peca peca(Position position) {
+		
+		if(!positionExists(position)) {
+			throw new BoardException("está posição não extiste no tabuleiro");
+		}
 		return pecas[position.getLinha()][position.getColuna()];
 	}
 	
 	public void placePeca(Peca peca, Position position) {
+		
+		if(thereIsAPiece(position)) {
+			throw new BoardException("Existe uma peça nesta posição");
+		}
 		pecas[position.getLinha()][position.getColuna()] = peca;
 		peca.position = position;
+	}
+	
+	private boolean positionExists(int linha, int coluna) {
+		return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas;
+	}
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getLinha(), position.getColuna()); // testando se a posição existe
+		
+	}
+	
+	public boolean thereIsAPiece(Position position) {
+		
+		if(!positionExists(position)) {
+			throw new BoardException("está posição não extiste no tabuleiro");
+		}
+		return peca(position) != null;
 	}
 
 }
